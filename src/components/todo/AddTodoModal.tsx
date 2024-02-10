480847de6ff781fcffbb72b72f8b512406991aa0
unknown <Monirul-Jim>
2024-02-10 21:22:07 +0600

    post method by redux

---
+++ src/components/todo/AddTodoModal.tsx
@@ -0,0 +1,80 @@
+import { FormEvent, useState } from "react";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { useAppDispatch } from "@/redux/features/hook";
+import { addTodo } from "@/redux/features/todoSlice";
+
+const AddTodoModal = () => {
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+  const dispatch = useAppDispatch();
+  const handleOnSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const taskDetails = {
+      title: task,
+      description: description,
+    };
+    dispatch(addTodo(taskDetails));
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="bg-primary-gradient text-xl font-semibold">
+          Add Todo
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+          <DialogDescription>
+            Add Your Task That You Want To Finish
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleOnSubmit}>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="task" className="text-right">
+                Tast
+              </Label>
+              <Input
+                onBlur={(e) => setTask(e.target.value)}
+                id="task"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Input
+                onBlur={(e) => setDescription(e.target.value)}
+                id="description"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button className="w-full" type="submit">
+                Save changes
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddTodoModal;
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,19 +1,21 @@
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add Todo</button>
-        <button>Filter</button>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal />
+        <TodoFilter />
       </div>
-      <div className="bg-primary-gradient w-full h-full rounded-xl  space-y-3 p-5">
+      <div className="bg-primary-gradient w-full h-full rounded-xl   p-5">
         {/* <div className="bg-white p-3">
           <p className="flex p-5 rounded-md text-2xl font-bold justify-center items-center">
             There is no task pending
           </p>{" "}
         </div> */}
-        <div className="bg-white p-5 w-full h-full rounded-lg">
+        <div className="bg-white p-5 w-full h-full space-y-3 rounded-lg">
           <TodoCard />
         </div>
       </div>
